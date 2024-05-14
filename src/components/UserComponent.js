7afd6df28962a236615ed1559e9f4318e3c717cf
MellityaSilviandroHening <mellityatya@gmail.com>
2024-05-15 00:31:48 +0700

    Update

---
+++ src/components/UserComponent.js
@@ -0,0 +1,33 @@
+// src/components/UserComponent.js
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchUser } from '../redux/slices/userSlice';
+
+function UserComponent() {
+  const userData = useSelector((state) => state.user.data);
+  const isLoading = useSelector((state) => state.user.loading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h2>User Details</h2>
+      {userData.map((user) => (
+        <div key={user.id}>
+          <span>Name: {user.name}</span>
+          <span> - </span>
+          <span>Email: {user.email}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default UserComponent;
